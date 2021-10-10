f4db5fa8ac3c79829f587fca3faf524be8f11bd6
cloudbug <cloudbug2020@gmail.com>
2021-10-10 10:14:35 +0200

    feat(lombok): add lombok

--- focusify-application/src/main/java/com/example/focusify/application/model/request/AddTodoRequest.java
+++ focusify-application/src/main/java/com/example/focusify/application/model/request/AddTodoRequest.java
@@ -1,10 +1,11 @@
 package com.example.focusify.application.model.request;
 
 import com.example.focusify.domain.todo.Status;
-import java.util.Objects;
 import javax.validation.constraints.NotNull;
+import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
+@Data
 public class AddTodoRequest {
 
   @Length(min = 1, max = 255)
@@ -15,55 +16,4 @@ public class AddTodoRequest {
 
   @NotNull
   private Status status;
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public Status getStatus() {
-    return status;
-  }
-
-  public void setStatus(Status status) {
-    this.status = status;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    AddTodoRequest that = (AddTodoRequest) o;
-    return Objects.equals(title, that.title) && Objects.equals(description,
-        that.description) && status == that.status;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(title, description, status);
-  }
-
-  @Override
-  public String toString() {
-    return "AddTodoRequest{" +
-        "title='" + title + '\'' +
-        ", description='" + description + '\'' +
-        ", status=" + status +
-        '}';
-  }
 }
--- focusify-application/src/main/java/com/example/focusify/application/model/request/UpdateTodoRequest.java
+++ focusify-application/src/main/java/com/example/focusify/application/model/request/UpdateTodoRequest.java
@@ -1,71 +1,11 @@
 package com.example.focusify.application.model.request;
 
 import com.example.focusify.domain.todo.Status;
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class UpdateTodoRequest {
-  private Long id;
   private String title;
   private String description;
   private Status status;
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public Status getStatus() {
-    return status;
-  }
-
-  public void setStatus(Status status) {
-    this.status = status;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    UpdateTodoRequest that = (UpdateTodoRequest) o;
-    return Objects.equals(id, that.id) && Objects.equals(title, that.title)
-        && Objects.equals(description, that.description) && status == that.status;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, title, description, status);
-  }
-
-  @Override
-  public String toString() {
-    return "UpdateTodoRequest{" +
-        "id=" + id +
-        ", title='" + title + '\'' +
-        ", description='" + description + '\'' +
-        ", status=" + status +
-        '}';
-  }
 }
