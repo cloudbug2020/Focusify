b10fd1ee2298f9873a016cee4b217c9d7b967cc5
cloudbug <cloudbug2020@gmail.com>
2021-10-16 10:53:03 +0200

    feat(health): add user resource health check

--- focusify-application/src/main/java/com/example/focusify/application/health/RestApiHealthCheck.java
+++ focusify-application/src/main/java/com/example/focusify/application/health/RestApiHealthCheck.java
@@ -1,6 +1,7 @@
 package com.example.focusify.application.health;
 
 import com.example.focusify.application.api.TodoResource;
+import com.example.focusify.application.api.UserResource;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import org.eclipse.microprofile.health.HealthCheck;
@@ -12,15 +13,21 @@ import org.eclipse.microprofile.health.Liveness;
 public class RestApiHealthCheck implements HealthCheck {
 
   @Inject TodoResource todoResource;
+  @Inject UserResource userResource;
 
   @Override
   public HealthCheckResponse call() {
-    final var hello = todoResource.hello();
+    final var todoHello = this.todoResource.hello();
+    final var userHello = this.userResource.hello();
 
-    if (hello.isEmpty() || hello.isBlank()) {
-      return HealthCheckResponse.named("Focusify REST Endpoint").down().build();
+    if (isNotNullOrEmpty(todoHello) || isNotNullOrEmpty(userHello)) {
+      return HealthCheckResponse.named("Focusify REST Endpoints").up().build();
     } else {
-      return HealthCheckResponse.named("Focusify REST Endpoint").up().build();
+      return HealthCheckResponse.named("Focusify REST Endpoints").down().build();
     }
+  }
+
+  private boolean isNotNullOrEmpty(String a) {
+    return a != null && !a.isEmpty() && !a.isBlank();
   }
 }
